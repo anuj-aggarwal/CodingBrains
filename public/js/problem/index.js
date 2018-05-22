be04423a3d1d304e1b28d487da9f44fa8a86fc31
Anuj Aggarwal <anuj.aggarwal63@gmail.com>
2018-05-22 21:34:43 +0530

    Add Submission Facility
    - Mock Submission at backend to send AC always
    - Make Submission frontend

--- public/js/problem/index.js
+++ public/js/problem/index.js
@@ -1,3 +1,41 @@
 $(() => {
     $("#submit-login-btn").click(() => $("#login-signup-btn").click());
+
+    const submitForm = $("#submit-form");
+    const resultContainer = $("#result-container");
+    submitForm.submit(event => {
+        event.preventDefault();
+        
+        const code = $(event.target).find("#code-text-area").val();
+        const language = $(event.target).find("#language-select").val();
+        
+        // Send API Request        
+        $.post(window.location.origin + window.location.pathname + "submit", {
+            code, language
+        }).then(data => {
+            showResult(resultContainer, data);
+        }).catch(err => {
+            console.error(err);
+        });
+    });
 });
+
+const showResult = (resultContainer, { verdict, timeUsed, memoryUsed }) => {
+    resultContainer.html("");
+    resultContainer.html(`
+        <div class="col-sm-12">
+            <h3 class="mb-3">Result</h3>
+        </div>
+        <div class="col-sm-4 mt-3">
+            <b>Verdict: </b> ${verdict}
+        </div>
+        <div class="col-sm-4 mt-3">
+            <b>Time Used: </b> ${timeUsed}s
+        </div>
+        <div class="col-sm-4 mt-3">
+            <b>Memory Used: </b> ${memoryUsed}MB
+        </div>
+    `);
+    resultContainer.addClass(`bg-${verdict.toLowerCase()}`);
+    resultContainer.show();
+}
--- routes/problems.js
+++ routes/problems.js
@@ -1,7 +1,9 @@
 const route = require("express").Router();
 
 // Import DB Models required
-const { Problem } = require("../models");
+const { Problem, Submission } = require("../models");
+// Utilities
+const { checkLoggedIn } = require("../utils/auth");
 
 // GET Route for a Problem
 route.get("/:id", async (req, res) => {
@@ -35,6 +37,46 @@ route.get("/:id/editorial", async (req, res) => {
 
     } catch (err) {
         console.error(err.stack);
+        res.sendStatus(500);
+    }
+});
+
+// Submit Function to Make API Request for Submission
+// Returns the Verdict, Time Used, and Memory Consumed
+// TODO: Make actual request, currently mocked
+const submit = (code, language) => {
+    return {
+        verdict: "AC",
+        timeUsed: 0.1,
+        memoryUsed: 100
+    };
+};
+
+// POST Route for Submission to the problem
+route.post("/:id/submit", checkLoggedIn, async (req, res) => {
+    try {
+        // Check if valid problem
+        const problem = await Problem.findById(req.params.id);
+        if (problem === null) {
+            return res.status(404).send("Problem not found!");
+        }
+
+        // Get required data
+        const { code, language } = req.body;
+
+        const result = submit(code, language);
+
+        // Make new submission
+        const submission = await Submission.create({
+            user: req.user,
+            problem: req.params.id,
+            code, language, ...result
+        });
+
+        res.send(result);
+
+    } catch (err) {
+        console.error(err);
         res.sendStatus(500);
     }
 });
